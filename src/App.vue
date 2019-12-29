9cc9671d8dd503146fb9d76bbbfc16ffec3d3777
Daulet Rakhmankul <daulet.rakhmankul@gmail.com>
2019-12-29 22:06:16 +0600

    serverden testterdi fetch arqyly zagruuzhat etedi

--- src/App.vue
+++ src/App.vue
@@ -12,16 +12,15 @@
         <p class="h5">Из интернета</p>
         <div class="d-flex justify-content-center">
           <div class="w-100 mx-2 border">
-            <input placeholder="Введите код" type="text" class="form-control rounded-0" />
-            <div v-show="true" class="list-group list-group-flush border-top text-primary">
-              <button type="button" class="list-group-item list-group-item-action py-1 border-0 text-primary">filosofia</button>
-              <button type="button" class="list-group-item list-group-item-action py-1 border-0 text-primary">dintanu</button>
-              <button type="button" class="list-group-item list-group-item-action py-1 border-0 text-primary">matematika</button>
-              <button type="button" class="list-group-item list-group-item-action py-1 border-0 text-primary">english</button>
-            </div>
+            <input placeholder="Введите код" v-model.trim="code" v-on:keyup.enter="loadFile" type="text" class="form-control rounded-0" />
           </div>
           <div>
-            <button type="button" class="btn btn-primary rounded-0">Загрузить</button>
+            <button type="button" v-on:click="loadFile" v-bind:disabled="downloading" class="btn btn-primary rounded-0" style="width: 95px">
+              <span v-if="downloading">
+                <font-awesome-icon :icon="['fas', 'spinner']" spin />
+              </span>
+              <span v-else>Загрузить</span>
+            </button>
           </div>
         </div>
       </div>
@@ -34,7 +33,7 @@
           <div class="w-100 mx-2">
             <input type="file" ref="jsonFile" class="form-control form-control-file rounded-0" />
           </div>
-          <button type="button" v-on:click="openFile" class="btn btn-primary rounded-0">Загрузить</button>
+          <button type="button" v-on:click="openFile" class="btn btn-primary rounded-0" style="width: 95px">Загрузить</button>
         </div>
       </div>
     </div>
@@ -55,16 +54,52 @@
     return Array.from({length: (end-start+1)}, (v, k) => k + start);
   };
 
+  let api_url = 'https://foxtest-retro.herokuapp.com/quiz/';
+
   export default {
     name: 'app',
     data() {
       return {
-        quiz_bank: {}
+        quiz_bank: {},
+        code: '',
+        downloading: false
       }
     },
     computed: {
     },
     methods: {
+      set_quiz_bank: function(quiz_bank) {
+        console.group('quiz_bank_info');
+        console.log(' - Тесты загружены -');
+        console.log('_v = ' + quiz_bank._v);
+        console.log('name = ' + quiz_bank.name);
+        console.log('code = ' + quiz_bank.code);
+        console.log('mode = ' + quiz_bank.mode);
+        console.log('created = ' + quiz_bank.created);
+        console.log('comment: ' + quiz_bank.comment);
+        console.log('Количество вопросов: ' + quiz_bank.quizes.length);
+        console.log('[' + Object.keys(quiz_bank).join(', ') + ']');
+        console.groupEnd('quiz_bank_info');
+
+        this.quiz_bank = quiz_bank;
+      },
+      loadFile: function() {
+        console.log(this.code);
+        if (this.code.length === 0)
+          return;
+        this.downloading = true;
+        fetch(api_url+this.code)
+                .then(response => response.json())
+                .then(data => {
+                  this.downloading = false;
+                  console.log('Файл загружен');
+                  this.set_quiz_bank(data)
+                })
+                .catch(error => {
+                  this.downloading = false;
+                  console.log('Request failed: ', error)
+                });
+      },
       openFile: function() {
         this.files = this.$refs.jsonFile.files;
         if (this.files.length === 0) {
@@ -80,18 +115,7 @@
       onReaderLoad: function(event) {
         let jsontext = event.target.result;
         let json = JSON.parse(jsontext);
-        console.group('openFile');
-        console.log(' - Тесты загружены -');
-        console.log('_v = ' + json._v);
-        console.log('name = ' + json.name);
-        console.log('code = ' + json.code);
-        console.log('mode = ' + json.mode);
-        console.log('created = ' + json.created);
-        console.log('comment: ' + json.comment);
-        console.log('Количество вопросов: ' + json.quizes.length);
-        console.log('[' + Object.keys(json).join(', ') + ']');
-        console.groupEnd('openFile');
-        this.quiz_bank = json;
+        this.set_quiz_bank(json)
       }
     },
     components: {
